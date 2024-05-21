3c0902f3e0a425043be45a23c4d0874e4cbe9b93
Patryk <patryk.swiderski.85@gmail.com>
2024-05-21 18:18:36 +0200

    Header and Quiz files added

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logoImg from "../assets/quiz-logo.png";
+
+function Header() {
+  return (
+    <header>
+      <img src={logoImg} alt="logo" />
+      <h1>ReactQuiz</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import QUESTIONS from "../questions.js";
+
+function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswer) => {
+      return [...prevUserAnswer, selectedAnswer];
+    });
+  }
+
+  return (
+    <div id="quiz">
+      <div id="questions">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Quiz;
